477a25fc64eed129643f1831c087b2681d690da5
mohamed-tarek-fouad <mt1348@fayoum.edu.eg>
2023-05-21 17:08:42 +0300

    application setup with local and google auth

---
+++ src/app.module.ts
@@ -0,0 +1,37 @@
+import { AuthModule } from './auth/auth.module';
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { AtGuard } from './common/guards';
+import { UsersModule } from './users/users.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    AuthModule,
+    UsersModule,
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: config.get('EMAIL_HOST'),
+          secure: false,
+          auth: {
+            user: config.get('EMAIL_USER'),
+            pass: config.get('EMAIL_PASSWORD'),
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot(),
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AtGuard,
+    },
+  ],
+})
+export class AppModule {}
